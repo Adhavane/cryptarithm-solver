a19b7cc6bbb4bff07a28cc6aacd247026dac9063
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-07-10 20:12:53 +0200

    Refactor main.pl to generate variables from operands

--- main.pl
+++ main.pl
@@ -11,8 +11,16 @@ parse_puzzle(Puzzle, Vars, Operators) :-
     Vars = [V],
     Operators = [].
 
+% Flatten a list of lists and remove duplicates
+generate_vars(Ops, Vars) :-
+    flatten(Ops, Vars1),
+    list_to_set(Vars1, Vars).
+% ?- flatten([[1,2],[3,4]], Vars). % Flatten a list of lists
+% ?- list_to_set([1,2,3,4,5,5,6], Vars). % Remove duplicates
+
 % solve(+Puzzle, -Solution)
 % ?- solve([[S,E,N,D],'+',[M,O,R,E],'#=',[M,O,N,E,Y]]).
 % ?- solve([[O,N,E],'+',[O,N,E],'+',[O,N,E],'#=',[T,W,O]]).
 solve(Puzzle) :-
-    parse_puzzle(Puzzle, Vars, Operators).
+    parse_puzzle(Puzzle, Operands, Operators),
+    generate_vars(Operands, Vars).
