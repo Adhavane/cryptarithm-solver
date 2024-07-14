ae9a4c8d34f0852c992ccb6c9952f71fc96c68aa
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-07-14 20:07:21 +0200

    feat: Add cripthmik.solve module with Solver and PrologSolver classes

---
+++ cripthmik/solve/__init__.py
@@ -0,0 +1,18 @@
+"""This module contains the classes that are used to solve the puzzles.
+
+Classes:
+    Solver: Abstract class for a solver that solves cryptarithms.
+    PrologSolver: A solver that uses Prolog to solve cryptarithms.
+
+Example:
+    >>> from cripthmik.solve import Solver
+    >>> class MySolver(Solver):
+    ...     def solve(self, cryptarithm: Cryptarithm) -> Generator[Solution, None, None]:
+    ...         pass
+"""
+
+
+from ._solver import Solver
+from ._prolog import PrologSolver
+
+__all__ = ["Solver", "PrologSolver"]
