acb6cddd3a20c9d6568d2961d5d8d6ae2e08f69c
Adhavane Moudougannane <adhavane.moudougannane@etu.utc.fr>
2024-07-21 17:27:57 +0200

    refactor: Add PrologSolver abstract class

--- cripthmik/solve/__init__.py
+++ cripthmik/solve/__init__.py
@@ -2,6 +2,7 @@
 
 Classes:
     Solver: Abstract class for a solver that solves cryptarithms.
+    PrologSolver: A solver that uses Prolog to solve cryptarithms.
     Enumerate: A solver that uses Enumeration to solve cryptarithms.
     GenerateAndTest: A solver that uses Generate and Test to solve cryptarithms.
 
@@ -13,8 +14,9 @@ Example:
 """
 
 
-from ._solver import Solver
 from ._enumerate import Enumerate
 from ._generate_and_test import GenerateAndTest
+from ._prolog import PrologSolver
+from ._solver import Solver
 
-__all__ = ["Solver", "Enumerate", "GenerateAndTest"]
+__all__ = ["Solver", "PrologSolver", "GenerateAndTest", "Enumerate"]
---
+++ cripthmik/solve/_prolog.py
@@ -0,0 +1,113 @@
+import multiprocessing as mp
+import tempfile
+from pathlib import Path
+from typing import Generator, List, Set, TypeAlias
+
+from pyswip import Prolog
+
+from ..utils import Cryptarithm
+from ._solver import Solution, Solver
+from abc import ABC, abstractmethod
+
+Rule: TypeAlias = str
+
+
+class PrologSolver(Solver, ABC):
+
+    def __init__(self):
+        super().__init__()
+
+    def _query(self, cryptarithm: Cryptarithm) -> Rule:
+        operators = cryptarithm.operators
+        operands = cryptarithm.words
+        query = ""
+
+        for i in range(len(operands) - 1):
+            query += f"[{','.join(operands[i])}], {operators[i]}, "
+        query += f"[{','.join(operands[-1])}]"
+
+        return f"solve([{query}])"
+
+    def solve(
+        self, cryptarithm: Cryptarithm,
+        allow_zero: bool = True, allow_leading_zero: bool = False
+    ) -> Generator[Solution, None, None]:
+        # Create a new process with a queue
+        result_channel = mp.Queue()
+        result_end = mp.Queue()
+        error_channel = mp.Queue()
+        error_end = mp.Queue()
+
+        # Start the worker process
+        worker = mp.Process(
+            target=self._solve_worker,
+            args=(
+                result_channel, result_end,
+                error_channel, error_end,
+                cryptarithm, allow_zero, allow_leading_zero,
+            ),
+        )
+        worker.start()
+
+        # Yield the queue
+        while True:
+            if result_end.empty() and error_end.empty():
+                if not result_channel.empty():
+                    yield result_channel.get()
+            else:
+                break
+
+        # Close the worker process
+        worker.join()
+
+        # Raise an exception if an error occurred
+        if not error_end.empty():
+            raise error_channel.get()
+
+        # Close the queues
+        result_channel.close()
+        result_end.close()
+        error_channel.close()
+        error_end.close()
+
+    def _solve_worker(
+        self,
+        result_channel: mp.Queue, result_end: mp.Queue,
+        error_channel: mp.Queue, error_end: mp.Queue,
+        cryptarithm: Cryptarithm, allow_zero: bool = True,
+        allow_leading_zero: bool = False,
+    ) -> None:
+        try:
+            # Generate rules and solve cryptarithm
+            prolog = Prolog()  # Create a Prolog engine
+
+            # Create a temporary file to store the rules
+            with tempfile.NamedTemporaryFile(mode="w", delete=False) as fp:
+                for rule in self._rules:
+                    fp.write(rule + ".\n")
+                fp.write("\n")
+
+                query = self._query(cryptarithm)
+                fp.write(f"{query} :- ")
+                fp.write(", ".join(map(str, self._query_rules(
+                    cryptarithm, allow_zero, allow_leading_zero))))
+                fp.write("\n")
+
+                fp.close()
+
+                # Consult the temporary file and query the Prolog engine
+                prolog.consult(Path(fp.name).as_posix())  # Convert to Posix path
+                for solution in prolog.query(query):
+                    result_channel.put(solution)
+                result_end.put(None)
+        except Exception as e:
+            error_channel.put(e)
+            error_end.put(None)
+
+        return
+
+    @abstractmethod
+    def _query_rules(
+        self, cryptarithm: Cryptarithm, allow_zero: bool, allow_leading_zero: bool
+    ) -> List[Rule]:
+        pass
